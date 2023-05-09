7e3abc1a2371da447459ad1ae773141156baf99d
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2023-05-09 02:59:30 -0400

    new jsonm

--- FoxBooks/scripts_FB.js
+++ FoxBooks/scripts_FB.js
@@ -9,22 +9,42 @@ var navLinks = document.getElementById("navLinks")
         }
 
 
-        let htmlString = "";
         const fetchData = async () => {
-          const response = await fetch("https://EthanOndreicka.github.io/SD330/FoxBooks/FB.json");
-          const data = await response.json();
-          for (let i = 0; i < data.courses.length; i++) {
-            htmlString += "<p>Course Name: " + data.courses[i][Object.keys(data.courses[i])[0]];
-            htmlString += "<br>Books: ";
-            for (let j = 0; j < data.courses[i].books.length; j++) {
-              htmlString += data.courses[i].books[j].title + ", ";
+            const response = await fetch("https://EthanOndreicka.github.io/SD330/FoxBooks/FB.json");
+            const data = await response.json();
+          
+            const courseDiv = document.getElementById("course");
+          
+            for (let i = 0; i < data.courses.length; i++) {
+              const course = data.courses[i];
+          
+              const courseName = course.courseName;
+              const courseNumber = course.courseNumber;
+              const professor = course.professor;
+          
+              const div = document.createElement("div");
+              div.classList.add("eachCourse-col");
+          
+              const h3 = document.createElement("h3");
+              h3.innerText = courseName;
+          
+              const p1 = document.createElement("p");
+              p1.innerText = courseNumber;
+          
+              const p2 = document.createElement("p");
+              p2.innerText = "Professor " + professor;
+          
+              div.appendChild(h3);
+              div.appendChild(p1);
+              div.appendChild(p2);
+          
+              courseDiv.appendChild(div);
             }
-            htmlString = htmlString.slice(0, -2); // remove last comma and space
-            htmlString += "</p>";
-          }
-          document.getElementById("info").innerHTML += htmlString;
-        };
-        fetchData();
+          };
+          
+          fetchData();
+          
+          
         
 
 
