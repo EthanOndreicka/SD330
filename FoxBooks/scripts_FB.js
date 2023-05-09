0ad84184dd519eba3e45e7d948cd71f0ae51997f
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2023-05-09 00:37:26 -0400

    ddd

--- FoxBooks/scripts_FB.js
+++ FoxBooks/scripts_FB.js
@@ -11,13 +11,21 @@ var navLinks = document.getElementById("navLinks")
 
 let htmlString = "";
     const fetchData = async () => {
-        const response = await fetch("https://EthanOndreicka.github.io/SD330/FB.json");
+        const response = await fetch("https://EthanOndreicka.github.io/SD330/FoxBooks/FB.json");
         const data = await response.json();
-       // htmlString += "<p>Course Name: " + data['courses']['CMPT_306']['books'];
-       // htmlString += "<p>Course Semester: " + data['courseName']['CMPT330']['course_semester'];
-        //htmlString += "<p>Book Author: " + data['books']['book']['book_author'];
-        //htmlString += "<p>Current Class: " + data['courseName']['CMPT330']['course_title'];
-        //htmlString += "<p> Current Class' Book: " + data['books']['SysBook']['book_title'];
+        const courses = data['courses'];
+        for (const course of courses){
+            htmlString += "<p>Course Name: " + course['CMPT_306'] + "</p>";
+            htmlString += "<p>Course Name: " + course['CMPT_330'] + "</p>";
+        }
+        htmlString += "<p>Course Name: " + data['courses']['CMPT_306']['books'];
         document.getElementById("info").innerHTML += htmlString;
     };
     fetchData();
+
+
+
+    // htmlString += "<p>Course Semester: " + data['courseName']['CMPT330']['course_semester'];
+        //htmlString += "<p>Book Author: " + data['books']['book']['book_author'];
+        //htmlString += "<p>Current Class: " + data['courseName']['CMPT330']['course_title'];
+        //htmlString += "<p> Current Class' Book: " + data['books']['SysBook']['book_title'];
